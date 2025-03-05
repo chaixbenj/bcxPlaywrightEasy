f31cd6e7c02e35690819f0446b14d1963f5df8d6
benjamin chaix <chaixben@gmail.com>
2025-03-05 10:04:03 +0100

    modif test

--- src/test/java/testCase/Tuto/TC_WebAndMobile.java
+++ src/test/java/testCase/Tuto/TC_WebAndMobile.java
@@ -1,6 +1,5 @@
 package testCase.Tuto;
 
-
 import bcx.automation.appium.AppiumDriver;
 import bcx.automation.report.Reporter;
 import bcx.automation.test.BaseTest;
@@ -8,38 +7,44 @@ import org.testng.annotations.Test;
 import pages.WebAndMobile.MessagesAndroid;
 import pages.WebAndMobile.MessagesWebFront;
 
-
 public class TC_WebAndMobile extends BaseTest {
-
 
     @Test(groups = {"tuto2"})
     public void testExchangeMessages() {
         try {
-            AppiumDriver.setupAndroid(this.testContext, "simu_nxs", "C:/temp/apk/app-debug.apk");
-
+            //Une page playwright est démarrée dans le @BeforeTest de la classe BaseTest
+            //On instancie notre page web messagerie et on navigate vers la page
             MessagesWebFront webFront = new MessagesWebFront(this.testContext);
             webFront.navigate();
+
+            //On démarre un simulateur android et on l'initialise avec l'apk de l'application
+            AppiumDriver.setupAndroid(this.testContext, "simu_nxs", "C:/temp/apk/app-debug.apk");
+
+            //On instancie notre page android messagerie
             MessagesAndroid android = new MessagesAndroid(this.testContext);
 
-
+            //Playwright envoie un sur le site Web
             webFront.sendMessage("Y'a quelqu'un sur le site ?");
 
+            //Appium+Selenium vérifie la récéption du message sur l'app Android et répond
             android.assertMessage("Y'a quelqu'un sur le site ?");
             android.sendMessage("Moi, je suis sur l'app android");
 
-            webFront.assertMessage("Moi, je suis sur l'app android");
+            //Playwright vérifie la récéption du message sur le site Web et répond
+            webFront.assertMessage("Moi, je suis sur l'app android FAIL"); //------------On fail volontairement la vérif web
             webFront.sendMessage("T'es un robot ? Qui te commande ?");
 
-            android.assertMessage("T'es un robot ? Qui te commande ?");
+            //Appium+Selenium vérifie la récéption du message sur l'app Android et répond
+            android.assertMessage("T'es un robot ? Qui te commande ? FAIL"); //------------On fail volontairement la vérif android
             android.sendMessage("APPIUM et SELENIUM. Et toi t'es un robot SELENIUM ?");
 
+            //Playwright vérifie la récéption du message sur le site Web et répond
             webFront.assertMessage("APPIUM et SELENIUM. Et toi t'es un robot SELENIUM ?");
             webFront.sendMessage("PLAYWRIGHT, papy ! J'ai pas ton time !");
 
+            //Appium+Selenium vérifie la récéption du message sur l'app Android et répond
             android.assertMessage("PLAYWRIGHT, papy ! J'ai pas ton time !");
             android.sendMessage("Papy ?! Pierre, c'est toi ? Le jour où tu commandes les apps natives fais signe gamin");
-
-            webFront.assertMessage("Papy ?! Pierre, c'est toi ? Le jour où tu commandes les apps natives fais signe gamin");
 
         } catch (Exception e) {
             this.getReport().log(Reporter.FAIL_STATUS, e);
